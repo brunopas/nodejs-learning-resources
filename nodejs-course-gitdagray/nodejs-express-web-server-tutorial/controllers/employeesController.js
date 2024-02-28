4905a2427ac7a2e2590661e9c19239cbf605a647
Bruno Pasquarelli Macedo <brunopas.macedo@gmail.com>
2024-02-27 21:37:59 -0300

    Initial Commit - ExpressJS Web Server Tutorial

---
+++ nodejs-course-gitdagray/nodejs-express-web-server-tutorial/controllers/employeesController.js
@@ -0,0 +1,101 @@
+const Employee = require("../model/Employee");
+
+const getAllEmployees = async (req, res) => {
+  const employees = await Employee.find();
+  if (!employees)
+    return res.status(204).json({ message: "No Employees found." });
+
+  res.json(employees);
+};
+
+const createNewEmployee = async (req, res) => {
+  if (!req?.body?.firstname || !req?.body?.lastname) {
+    return res
+      .status(400)
+      .json({ message: "First and Last Names are required." });
+  }
+
+  try {
+    const result = await Employee.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const updateEmployee = async (req, res) => {
+  if (!req?.body?.id) {
+    return res.status(400).json({ message: "ID parameter is required." });
+  }
+
+  try {
+    const employee = await Employee.findOne({ _id: req.body.id });
+
+    if (!employee) {
+      return res
+        .status(204)
+        .json({ message: `No Employee matches ID ${req.body.id}.` });
+    }
+
+    if (req.body?.firstname) employee.firstname = req.body.firstname;
+    if (req.body?.lastname) employee.lastname = req.body.lastname;
+
+    const result = await employee.save();
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const deleteEmployee = async (req, res) => {
+  if (!req?.body?.id) {
+    return res.status(400).json({ message: "Employee ID required." });
+  }
+
+  try {
+    const employee = await Employee.findOne({ _id: req.body.id });
+
+    if (!employee) {
+      return res
+        .status(204)
+        .json({ message: `No Employee matches ID ${req.body.id}.` });
+    }
+
+    const result = await employee.deleteOne({ _id: req.body.id });
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getEmployee = async (req, res) => {
+  if (!req?.params?.id) {
+    return res.status(400).json({ message: "Employee ID required." });
+  }
+
+  try {
+    const employee = await Employee.findOne({ _id: req.params.id });
+
+    if (!employee) {
+      return res
+        .status(204)
+        .json({ message: `No Employee matches ID ${req.params.id}.` });
+    }
+
+    res.json(employee);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = {
+  getAllEmployees,
+  createNewEmployee,
+  updateEmployee,
+  deleteEmployee,
+  getEmployee,
+};
---
+++ nodejs-course-gitdagray/nodejs-express-web-server-tutorial/controllers/registerController.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcrypt");
+const User = require("../model/User");
+
+const handleNewUser = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
+
+  // Check for duplicate usernames in the DB
+  const duplicate = await User.findOne({ username: user }).exec();
+  if (duplicate) return res.sendStatus(409); // Conflict
+
+  try {
+    // Encrypt the password
+    const hashedPwd = await bcrypt.hash(pwd, 10);
+
+    // Create and store the new user
+    await User.create({
+      username: user,
+      password: hashedPwd,
+    });
+
+    res.status(201).json({ success: `New user ${user} created!` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { handleNewUser };
